dca03087780e06cd4fa1731403bb0147e7c39496
sherrillmix <sherrillmix@users.noreply.github.com>
2015-12-30 21:38:17 -0500

    Move to suffic executable

--- makefile
+++ makefile
@@ -1,14 +1,17 @@
-all: treefind
+all: suffic
 .PHONY: all
 
-treefind: tree.c main.c tests.bash testtree.c makefile
+suffic: tree.c main.c makefile
+	gcc -c tree.c -lz -lpthread
+	gcc -o suffic main.c tree.o -lz -lpthread
+
+
+test: tree.c main.c tests.bash testtree.c makefile
 	gcc -Wall -c tree.c -lz -lpthread -coverage
-	gcc -Wall -o treefind main.c tree.o -lz -lpthread -coverage
+	gcc -Wall -o suffic main.c tree.o -lz -lpthread -coverage
 	gcc -Wall -o testtree tree.o testtree.c -lz -lpthread -coverage
 	./testtree
 	bash tests.bash
 	gcov main.c tree.h tree.c
-	gcc -c tree.c -lz -lpthread
-	gcc -o treefind main.c tree.o -lz -lpthread
-
-
+	rm suffic tree.o
+	make suffic
--- tests.bash
+++ tests.bash
@@ -38,18 +38,18 @@ CTCTCTCTCTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 END
 
 
-./treefind 2>/dev/null && { echo "Missing files did not fail"; exit 1; }
-./treefind $refFile 2>/dev/null && { echo "Missing file did not fail"; exit 1; }
-./treefind -h $refFile $fastqFile 2>/dev/null && { echo "-h did not fail"; exit 1; }
-./treefind -z $refFile $fastqFile 2>/dev/null && { echo "Weird argument did not fail"; exit 1; }
-./treefind -s 10 $refFile $fastqFile 2>/dev/null && { echo "Weird argument did not fail"; exit 1; }
-./treefind -$'\05' $refFile $fastqFile 2>/dev/null && { echo "Weird argument did not fail"; exit 1; }
-./treefind $refFile $fastqFile 2>/dev/null || { echo "Running with 2 args failed"; exit 1; }
-./treefind $refFile $fastqFile -t4 2>/dev/null || { echo "Running with 4 cores failed"; exit 1; }
-./treefind $refFile $fastqFile -m2 2>/dev/null || { echo "Running with 2 mismatch failed"; exit 1; }
-./treefind $refFile $fastqFile -m2 -t4 2>/dev/null || { echo "Running with 2 mismatch and 4 thread failed"; exit 1; }
+./suffic 2>/dev/null && { echo "Missing files did not fail"; exit 1; }
+./suffic $refFile 2>/dev/null && { echo "Missing file did not fail"; exit 1; }
+./suffic -h $refFile $fastqFile 2>/dev/null && { echo "-h did not fail"; exit 1; }
+./suffic -z $refFile $fastqFile 2>/dev/null && { echo "Weird argument did not fail"; exit 1; }
+./suffic -s 10 $refFile $fastqFile 2>/dev/null && { echo "Weird argument did not fail"; exit 1; }
+./suffic -$'\05' $refFile $fastqFile 2>/dev/null && { echo "Weird argument did not fail"; exit 1; }
+./suffic $refFile $fastqFile 2>/dev/null || { echo "Running with 2 args failed"; exit 1; }
+./suffic $refFile $fastqFile -t4 2>/dev/null || { echo "Running with 4 cores failed"; exit 1; }
+./suffic $refFile $fastqFile -m2 2>/dev/null || { echo "Running with 2 mismatch failed"; exit 1; }
+./suffic $refFile $fastqFile -m2 -t4 2>/dev/null || { echo "Running with 2 mismatch and 4 thread failed"; exit 1; }
 
-cmd="./treefind $refFile $fastqFile -o $tmpDir/test -m 1" 
+cmd="./suffic $refFile $fastqFile -o $tmpDir/test -m 1" 
 echo $cmd
 $cmd 2>/dev/null || { echo "Running with output file set to /tmp failed"; exit 1; }
 zcat $tmpDir/test_match.fastq.gz |grep seq1 >/dev/null || { echo "seq1 not found in match"; exit 1; }
@@ -59,7 +59,7 @@ zcat $tmpDir/test_partial.fastq.gz |grep seq3 >/dev/null && { echo "seq3 erroneo
 zcat $tmpDir/test_match.fastq.gz |grep seq2 >/dev/null || { echo "seq4 two sided not found in match"; exit 1; }
 zcat $tmpDir/test_partial.fastq.gz |grep seq4 >/dev/null && { echo "seq4 erroneously found in partial"; exit 1; }
 
-cmd="./treefind $refFile $fastqFile -o $tmpDir/test -m 0"
+cmd="./suffic $refFile $fastqFile -o $tmpDir/test -m 0"
 echo $cmd
 $cmd 2>/dev/null || { echo "Running with mismatch 0 failed"; exit 1; }
 zcat $tmpDir/test_match.fastq.gz |grep seq1 >/dev/null && { echo "seq1 erroneously found in match"; exit 1; }
@@ -69,7 +69,7 @@ zcat $tmpDir/test_partial.fastq.gz |grep seq3 >/dev/null && { echo "seq3 erroneo
 zcat $tmpDir/test_match.fastq.gz |grep seq4 >/dev/null && { echo "seq3 erroneously found in match"; exit 1; }
 zcat $tmpDir/test_partial.fastq.gz |grep seq4 >/dev/null && { echo "seq3 erroneously found in partial"; exit 1; }
 
-cmd="./treefind $refFile $fastqFile -o $tmpDir/test -m 0 -l 11" 
+cmd="./suffic $refFile $fastqFile -o $tmpDir/test -m 0 -l 11" 
 echo $cmd
 $cmd 2>/dev/null || { echo "Running with output file set to /tmp failed"; exit 1; }
 zcat $tmpDir/test_match.fastq.gz |grep seq1 >/dev/null && { echo "seq1 erroneously found in match"; exit 1; }
@@ -79,5 +79,5 @@ zcat $tmpDir/test_partial.fastq.gz |grep seq1 >/dev/null && { echo "seq4 erroneo
 
 longFileName=$tmpDir/$(printf '0123456789%.0s' {1..101})
 #probably can't copy because filename too long but that means safe
-(cp $refFile $longFileName 2>/dev/null || ./treefind $longFileName $fastqFile 2>/dev/null) && { echo "Long file name did not fail"; exit 1; }
+(cp $refFile $longFileName 2>/dev/null || ./suffic $longFileName $fastqFile 2>/dev/null) && { echo "Long file name did not fail"; exit 1; }
 exit 0
