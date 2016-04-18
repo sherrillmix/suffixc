f6ff25eaad034580a0995b0af0bcbc454462d8c6
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-04-18 12:14:29 -0400

    Organize and remove -t from usage message

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main (int argc, char *argv[]){
   char **outFiles;
   int params[2];
   //\n  -t: (optional) specify how many threads to use (default: 2)
-  sprintf(usage,"Usage: %.1000s ref.fa reads.fastq [-m 2] [-t 4]\n  first argument: a reference sequence in a fasta file (if this is much more than 10kb then we could get memory problems)\n  second argument: a fastq file containing the reads to search\n  -o: (optional) the output prefix for outfiles (default: suffixcOut)\n  -l: (optional) specify how long a match has to be considered a partial match. A partial match on both ends counts as a match (default: 20)\n  -m: (optional) specify how many mismatches to tolerate (default: 0)\n  -h: (optional) display this message and exit\n",argv[0]);
+  sprintf(usage,"Usage: %.1000s ref.fa reads.fastq [-o suffixcOut] [-l 20] [-m 0] [-h]\n  first argument: a reference sequence in a fasta file (if this is much more than 10kb then we could get memory problems)\n  second argument: a fastq file containing the reads to search\n  -o: (optional) the output prefix for outfiles (default: suffixcOut)\n  -l: (optional) specify how long a match has to be considered a partial match. A partial match on both ends counts as a match (default: 20)\n  -m: (optional) specify how many mismatches to tolerate (default: 0)\n  -h: (optional) display this message and exit\n",argv[0]);
   //t:
   while ((c = getopt (argc, argv, "hm:o:l:")) != -1){
     switch (c){
